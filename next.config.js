2c22ce843568e70d6ac84839bd11a829c0fe6c20
Ifenna-Pascal <ifennamonanu70@gmail.com>
2023-02-04 08:50:57 +0100

    initial setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['src'], // Only run ESLint on the src directory during production builds (next build)
+  },
 }
 
 module.exports = nextConfig
